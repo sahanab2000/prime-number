d32a97c3f5ef73b01023a467f378a25f7ca8725d
sahanab2000 <85185990+sahanab2000@users.noreply.github.com>
2022-10-14 19:43:08 +0530

    Add files via upload

---
+++ prime no.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Demo
+{
+    class MyApplication
+    {
+        public static void Main()
+        {
+            int n = 5, a = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    a++;
+                }
+            }
+            if (a == 2)
+            {
+                Console.WriteLine("{0} is a Prime Number", n);
+            }
+            else
+            {
+                Console.WriteLine("Not a Prime Number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
